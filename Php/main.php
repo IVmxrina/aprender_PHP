3c81a1124217676f02bcba0e39f1c69f6c51b189
AndresXW5 <73656983+AndresXW5@users.noreply.github.com>
2022-11-25 18:20:16 +0100

    Avances en main.php | PHP

--- Php/main.php
+++ Php/main.php
@@ -18,8 +18,34 @@ echo "El modelo introducida es " . $Modelo . "<br>";
 echo "EL color introducida es " . $Color;
 
 
+// 3º Hacemos un ' include '  o un  ' require ' para añadir los datos de la conexion a la BD 
 
+include '/BD/bd.php';
 
+// 4º Ahora comprobamos que la conexion a la base de datos se a realizado correctamente
+// Esto se hace mediante un if; si si la conexion es incorrecta nos devuelve un error;
+// Si la conexion es bien continuara el if y podemos hacer consultas a la BD
+
+if(!$conexion){
+    die("No se ha podido realizar la conexion_".mysqli_connect_error()."<br>");
+}else{
+   
+   
+    // mysqli_set_charset($con, "utf8");
+    // echo "Se ha establecido correctamente la conexion a la base de datos <br>";
+
+    // $sql="INSERT INTO `daw2`.`Alumnos` (`correo`, `contraseña`) VALUES ('".$correo."', '".$contraseña."')";
+    
+    // $consulta=mysqli_query($con,$sql);
+
+    // if(!$consulta){
+    //     die("No se ha podido realizar el insert");
+    // }else{
+    //     echo "El insert se ha realziado correctamente.";
+    // }
+
+    
+}
 
 
 ?>
